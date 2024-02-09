3470f81b871ab06d0f35859be20c58460f53547c
Sebastian <sebastian_rodas26@outlook.es>
2024-02-08 23:54:42 -0600

    Add text menubar & fix the text heading level

--- src/components/TipTapEditor.tsx
+++ src/components/TipTapEditor.tsx
@@ -1,29 +1,36 @@
-'use client'
-import React from 'react'
-import { EditorContent, useEditor } from '@tiptap/react'
-import {StarterKit} from '@tiptap/starter-kit'
+"use client";
+import React from "react";
+import { EditorContent, useEditor } from "@tiptap/react";
+import { StarterKit } from "@tiptap/starter-kit";
+import TipTapMenuBar from "./TipTapMenuBar";
+import { Button } from "./ui/button";
+import { MainButton } from "./ui/mainButton";
 
-type Props = {}
+type Props = {};
 
 const TipTapEditor = (props: Props) => {
-      
-      const [editorState, setEditorState] = React.useState('');
-      
-      const editor = useEditor({
-            autofocus: true,
-            extensions: [StarterKit], 
-            content: editorState,
-            onUpdate: ({editor}) => {
-                  setEditorState(editor.getHTML());
-            } 
-      })
-      return (
-    <div>
-      <div> 
-            <EditorContent editor={editor}/>
-      </div>
-    </div>
-  )
-}
+  const [editorState, setEditorState] = React.useState("");
 
-export default TipTapEditor
+  const editor = useEditor({
+    autofocus: true,
+    extensions: [StarterKit],
+    content: editorState,
+    onUpdate: ({ editor }) => {
+      setEditorState(editor.getHTML());
+    },
+  });
+  return (
+    <>
+      <div className="flex">
+           {editor &&  <TipTapMenuBar editor={editor}/>}
+            <MainButton>Saved</MainButton>
+      </div>
+        <div className="prose">
+          <EditorContent editor={editor} />
+        </div>
+     
+    </>
+  );
+};
+
+export default TipTapEditor;
